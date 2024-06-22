fe441a80c90ce5bfd82abab3282514acadb1e9a3
GavrilStefan-Dorian <stefan.dorian.gavril@gmail.com>
2024-06-22 12:38:00 +0300

    RSS has atomlink for validator sites

--- src/scripts/rssFeed.js
+++ src/scripts/rssFeed.js
@@ -7,11 +7,12 @@ async function generateRSSFeed() {
     const pubDate = new Date().toUTCString();
 
     let rssContent = `<?xml version="1.0" encoding="UTF-8" ?>
-    <rss version="2.0">
+    <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
       <channel>
         <title>Book Review RSS Feed</title>
         <description>Latest reviews and book updates</description>
         <link>https://boo.com</link>
+        <atom:link href="https://localhost:3000/rssFeed" rel="self" type="application/rss+xml" />
         <language>en-us</language>
         <pubDate>${pubDate}</pubDate>
         <lastBuildDate>${pubDate}</lastBuildDate>
